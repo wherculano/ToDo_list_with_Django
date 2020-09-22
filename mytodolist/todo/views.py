b8dcbc27d202e560189673c5d5d90aab0f3965f9
wherculano <wagherculano@hotmail.com>
2020-09-21 21:02:46 -0300

    Edited list items with templates close #11

--- mytodolist/todo/views.py
+++ mytodolist/todo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render  # noqa
 from django.http import HttpResponse  # noqa
+from .models import TodoItem
 
 
 # Create your views here.
 def todoView(request):
-    return render(request, 'todo.html')
+    all_todo_items = TodoItem.objects.all()
+    return render(request, 'todo.html',
+                  {'all_todo_items': all_todo_items})
