79a40000f4b96ff36d3e983b8b4ba2c95531ec3e
wherculano <wagherculano@hotmail.com>
2020-09-22 11:39:00 -0300

    Extracted variables using decouple close #17

--- mytodolist/settings.py
+++ mytodolist/settings.py
@@ -13,18 +13,20 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config, Csv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n=xuqqhet)9%nu#!)9zty92&^@9+xfgy=dl^e$o8q@3&#p!62$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
